a86ee12d24c517d3492484e5f719b40f9b2170b0
ricardoxd <yoricardoxd@gmail.com>
2019-04-16 15:57:15 -0400

    FIX

--- resources/views/layouts/data-laravel-echo.blade.php
+++ resources/views/layouts/data-laravel-echo.blade.php
@@ -2,7 +2,7 @@
     window.Laravel = {!! json_encode([
         'cliente' => auth()->guard('web')->check() ? (string)auth()->guard('web')->user()->_id : null,
         'telefono' => auth()->guard('web')->check() ? (string)auth()->guard('web')->user()->Telefono : null,
-        'evento' => auth()->guard('web')->check() ? 'evento' : null
+        'evento' => auth()->guard('web')->check() ? 'evento' : null,
         'empresa' => auth()->guard('web')->check() ? 'empresa' : null
     ]) !!};
 </script>
