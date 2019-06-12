b478c15127e5fd85a4935f87dbd8f7b01e264876
Gabriel Ortega(GaboDevelop) <gabriel.enr1998@gmail.com>
2019-06-12 16:48:46 -0400

    seccion de invitacion, carga y descargar de imagenes/pdf

--- resources/js/components/menuApps/Invitacion.js
+++ resources/js/components/menuApps/Invitacion.js
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSync } from "@fortawesome/free-solid-svg-icons";
 import axios from "axios";
 import swal from "sweetalert2";
-
+import DeviceOrientation, { Orientation } from "react-screen-orientation";
 library.add(faSync);
 
 export default class Invitacion extends Component {
@@ -13,28 +13,99 @@ export default class Invitacion extends Component {
         super(props);
         this.state = {
             url: props.url,
-            invitaciones: props.invitaciones
+            invitaciones: JSON.parse(props.invitaciones),
+            isLoading: false
         };
     }
 
     render() {
-        let algo;
-        let invitacionesArray = JSON.parse(this.state.invitaciones);
-        return (
-            <div>
-                <div className="mb-4">
-                    <img src={invitacionesArray[0].PathImg} />
-                </div>
+        let url = this.state.url;
 
-                <div>
-                    <a
-                        className="btn btn-danger"
-                        href={invitacionesArray[0].PathPdf}
-                    >
-                        <i className="fas fa-file-pdf" />
-                        &nbsp;&nbsp;Descargar PDF
-                    </a>
-                </div>
+        let { invitaciones } = this.state;
+
+        var prueba = invitaciones[0].PathImg.split("http://localhost:8000");
+        console.log(prueba);
+        return (
+            <div className="container-fluid">
+                <DeviceOrientation>
+                    <Orientation orientation="landscape" alwaysRender={false}>
+                        {invitaciones[0] ? (
+                            <div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        {invitaciones[0].PathImg ? (
+                                            <img
+                                                src={invitaciones[0].PathImg}
+                                                className="invitacion-img"
+                                            />
+                                        ) : (
+                                            ""
+                                        )}
+                                    </div>
+                                </div>
+
+                                <div className="row  mt-4">
+                                    <div className="col-12 text-center">
+                                        {invitaciones[0].PathPdf ? (
+                                            <a
+                                                href={invitaciones[0].PathPdf}
+                                                target="_blank"
+                                            >
+                                                <button className="btn btn-sm btn-danger">
+                                                    Descargar Invitación PDF
+                                                </button>
+                                            </a>
+                                        ) : (
+                                            ""
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        ) : (
+                            <div>
+                                Rote su dispositivo en modo vertical para ver la
+                                tarjeta de invitación
+                            </div>
+                        )}
+                    </Orientation>
+
+                    <Orientation orientation="portrait" alwaysRender={false}>
+                        {invitaciones[1] ? (
+                            <div>
+                                <div className="row">
+                                    <div className="col-12">
+                                        <img
+                                            src={invitaciones[1].PathImg}
+                                            className="img-fluid"
+                                        />
+                                    </div>
+                                </div>
+
+                                <div className="row  mt-4">
+                                    <div className="col-12 text-center">
+                                        {invitaciones[1].PathPdf ? (
+                                            <a
+                                                href={invitaciones[1].PathPdf}
+                                                target="_blank"
+                                            >
+                                                <button className="btn btn-sm btn-danger">
+                                                    Descargar Invitación PDF
+                                                </button>
+                                            </a>
+                                        ) : (
+                                            ""
+                                        )}
+                                    </div>
+                                </div>
+                            </div>
+                        ) : (
+                            <div>
+                                Rote su dispositivo en modo horizontal para ver
+                                la tarjeta de invitación
+                            </div>
+                        )}
+                    </Orientation>
+                </DeviceOrientation>
             </div>
         );
     }
