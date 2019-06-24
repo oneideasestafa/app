d135858c32fd6ba3188ff5d540b74cbb8d02edba
Gabriel Ortega(GaboDevelop) <gabriel.enr1998@gmail.com>
2019-06-24 12:49:18 -0400

    test ruta post api perfil

--- resources/js/components/components/MenuApps/CambiarDatos.js
+++ resources/js/components/components/MenuApps/CambiarDatos.js
@@ -310,6 +310,7 @@ export default class CambiarDatos extends Component {
         });
 
         let {idUsuario,pais, telefono,  fechaNacimiento, equipo, sexo, civil, nombre, apellido, url, fotonew, tipofoto} = this.state;
+        console.log("abajo se envia un axios.post de api/clientes/editar/perfil/");
         axios.post('api/clientes/editar/perfil/', { idUsuario,pais, telefono,  fechaNacimiento, equipo, sexo, civil, nombre, apellido, fotonew, tipofoto})
             .then(res => {
                 self.setState({
