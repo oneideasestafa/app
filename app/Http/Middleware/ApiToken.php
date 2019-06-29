33d51279eeaf5e0327394a9133deda0f147339b6
Javier Martin <jamartincelis@gmail.com>
2019-06-29 19:52:50 -0400

    Validación de cabecera y baerer token en el Middleware
     -Se agrega la función getBearerToken en el middleware ApiToken.php.
     -Se modifica la función handle en el middleware ApiToken.php para validar el token de acceso que viaja en la cabecera.
     -Se agregan las rutas /login-new(temporal), /registro y /logout. Se deben verificar si se quedaran.

--- app/Http/Middleware/ApiToken.php
+++ app/Http/Middleware/ApiToken.php
@@ -3,23 +3,45 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use App\User;
 
 class ApiToken
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-      if($request->header('Authorization')){
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @return mixed
+   */
+  public function handle($request, Closure $next)
+  {
+    $token = $this->getBearerToken($request);
+    if($token){
+      $user = User::where('api_token',$token)->first();
+      if($user) {
         return $next($request);
-      }
+      }  
       return response()->json([
-        'message' => 'Not a valid API request.',
-      ]);
+        'message' => 'Not a valid access token.',
+      ]);        
     }
+    return response()->json([
+      'message' => 'Not a valid api request.',
+    ]);
+  }
+
+  /**
+   * Obtiene el token de acceso de la cabecera, ya que viene en el formato "Bearer xxxxxx"
+   * */
+  function getBearerToken($request) {
+      $headers = trim($request->header('Authorization'));
+      // HEADER: Get the access token from the header
+      if (!empty($headers)) {
+          if (preg_match('/Bearer\s(\S+)/', $headers, $matches)) {
+              return $matches[1];
+          }
+      }
+      return null;
+  }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,10 +16,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-//defifinir los puntos de entrada para los endpoints de usuarios
+// Permite defifinir los puntos de entrada para los endpoints de usuarios
 Route::group(['prefix' => 'usuarios'], function() {
-	//Route::post('/login', 'LoginController@login');
-    Route::post('/login','Api\AuthController@postLogin');
+	Route::post('/login', 'LoginController@login');
+    Route::post('/login-new','Api\AuthController@postLogin');
+    Route::post('/registro','Api\AuthController@postRegister');
+    Route::post('/logout','Api\AuthController@postLogout');
 });
 
 // Permite difinir los puntos de entrada para los endpoints de eventos
@@ -37,9 +38,6 @@ Route::group(['prefix' => 'clientes','middleware' => 'ApiToken'], function() {
     Route::post('/clubs-perfil', 'ClienteController@getClubsPais');
     Route::post('/editar/perfil', 'ClienteController@editarCliente');
     Route::post('/editar/cambiar-clave', 'ClienteController@cambiarClave');
-
-
 	Route::get('/estado-civil', 'ClienteController@getEstadoCivil');
     Route::post('/registro', 'ClienteController@crearCliente');
-    //Route::post('/club', 'RegistroController@ajaxPostClubs');
 });
