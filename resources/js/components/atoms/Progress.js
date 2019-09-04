963de5782f66472685abbf558bd24f52132697f5
Gerardo <Garcia>
2019-09-04 11:26:26 -0400

    Download process functionality

--- resources/js/components/atoms/Progress.js
+++ resources/js/components/atoms/Progress.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import classnames from 'classnames';
 
-function Progress(props) {
+function Progress({ progress, color = 'success'}) {
+  const classNames = classnames('progress-bar', `bg-${color}`);
+
   return (
     <div className="progress">
       <div 
-        className="progress-bar bg-success" 
+        className={classNames}
         role="progressbar" 
-        style={{width: `${props.progress}%`}}
-        aria-valuenow={`${props.progress}`}
+        style={{width: `${progress}%`}}
+        aria-valuenow={`${progress}`}
         aria-valuemin="0" 
         aria-valuemax="100">
       </div>
--- resources/js/components/molecules/FileDownload.js
+++ resources/js/components/molecules/FileDownload.js
@@ -1,16 +1,63 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Progress from '../atoms/Progress';
+import WebTorrent from 'webtorrent';
 
 function FileDownload (props) {
+  const [ isReady, setReady ] = useState(false);
+  const [ error, setError ] = useState(null);
+  const [ info, setInfo ] = useState({ progress: 0, bytes: 0 });
+  const client = new WebTorrent();
+
+  useEffect(() => { 
+    if (!props.file.magnetURI)
+      return;
+    
+    const torrent = client.add(props.file.magnetURI);
+    
+    torrent.on('ready', () => setReady(true));
+    
+    torrent.on('error', error => setError(error));
+    
+    torrent.on('download', bytes => setInfo({
+      downloadSpeed: torrent.downloadSpeed / 1000,
+      pieceLength: torrent.pieceLength / 1000,
+      progress: torrent.progress * 100,
+      bytes: bytes / 1000,
+    }));
+
+    torrent.on('done', () => {
+
+    });
+
+  }, [props.file.magnetURI]);
+
+  const color = error ? 'danger' : 'success';
+
   return (
     <div className="download-box">
       <div className="download-info">
         <div className="info-header">
           <label>{props.file.name}</label>
+          {!props.file.magnetURI &&
+            <p>
+              El archivo aún no está disponible para descargar,
+              intentelo de nuevo más tarde
+            </p>
+          }
+          {isReady &&
+            <p>
+              {`${info.downloadSpeed}Kb/s - ${info.bytes}Kb de ${info.pieceLength}Kb`}
+            </p>
+          }
         </div>
-        <div className="info-progress">
-          <Progress progress={50}/>
-        </div>
+        {props.file.magnetURI && 
+          <div className="info-progress">
+            <Progress 
+              color={color}
+              progress={info.progress}
+            />
+          </div>
+        }
       </div>
       <div className="download-actions">
 
