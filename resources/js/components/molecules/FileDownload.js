94d3696b3b793884d4f3787f97f9124a58accc6a
Gerardo <Garcia>
2019-09-04 17:13:50 -0400

    Saving file in app's specific storage

--- resources/js/components/molecules/FileDownload.js
+++ resources/js/components/molecules/FileDownload.js
@@ -1,35 +1,58 @@
 import React, { useState, useEffect } from 'react';
 import Progress from '../atoms/Progress';
 import WebTorrent from 'webtorrent';
+import { connect } from 'react-redux';
 
 function FileDownload (props) {
   const [ isReady, setReady ] = useState(false);
   const [ isDownloaded, setDownloaded ] = useState(false);
   const [ error, setError ] = useState(null);
-  const [ info, setInfo ] = useState({ progress: 0, size: 0 });
-  const client = new WebTorrent();
-
-  useEffect(() => {
-    if (!props.file.magnetURI)
-      return;
-    
+  const [ info, setInfo ] = useState({});
+  
+  useEffect(() => {    
+    const client = new WebTorrent();
     const torrent = client.add(props.file.magnetURI);
     
     torrent.on('ready', () => setReady(true));
     
     torrent.on('error', error => setError(error));
     
-    torrent.on('download', bytes => {
-      setInfo({
-        progress: torrent.progress * 100,
-        downloaded: Math.round(torrent.downloaded / 1000),
-        downloadSpeed: Math.round(torrent.downloadSpeed / 1000),
-      })
+    torrent.on('download', bytes => setInfo({
+      progress: torrent.progress * 100,
+      downloaded: Math.round(torrent.downloaded / 1000),
+      downloadSpeed: Math.round(torrent.downloadSpeed / 1000),
+    }));
+
+    torrent.on('done', () => {
+      setDownloaded(true);
+      torrent.files.forEach(file => file.getBlob(saveDownloadedFile));
     });
 
-    torrent.on('done', () => setDownloaded(true));
-
   }, [props.file.magnetURI]);
+
+  function saveDownloadedFile (err, blob) {
+    if (err)
+      return console.log('saveDownloadedFile:', err);
+    
+    requestFileSystem(LocalFileSystem.PERSISTENT, 0, fs => {
+      const { Empresa_id, _id } = props.event;
+      const { name } = props.file;
+
+      fs.root.getDirectory(Empresa_id, { create: true }, companyDir => {
+        companyDir.getDirectory(_id, { create: true }, eventDir => {
+          eventDir.getFile(`${name}`, { create: true, exclusive: false}, fe => {
+            fe.createWriter(fw => {
+              
+              fw.onwriteend = () => console.log('End!!');
+              fw.onerror = err => console.log('error:', err);
+              fw.write(blob);
+    
+            }, err => console.log('createWriter:', err));
+          }, err => console.log('getFile:', err));     
+        }, err => console.log('getEventDirectory:', err));
+      }, err => console.log('getCompanyDirectory:', err));
+    }, err => console.log('requestFileSystem:', err));
+  }
 
   const color = error ? 'danger' : 'success';
 
@@ -69,4 +92,8 @@ function FileDownload (props) {
   );
 }
 
-export default FileDownload;
+const mapStateToProps = state => ({
+  event: state.events.current
+});
+
+export default connect(mapStateToProps)(FileDownload);
