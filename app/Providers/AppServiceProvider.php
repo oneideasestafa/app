4b651392ef82252282555a49ada4176d6ff5e397
GerardoAGL96 <gerardoagl96@gmail.com>
2019-10-27 15:44:11 -0400

    Social auth backend implementation

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,27 +2,38 @@
 
 namespace App\Providers;
 
+use App\Services\SocialUserResolver;
+use Coderello\SocialGrant\Resolvers\SocialUserResolverInterface;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+  /**
+   * All of the container bindings that should be registered.
+   *
+   * @var array
+   */
+  public $bindings = [
+    SocialUserResolverInterface::class => SocialUserResolver::class,
+  ];
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
+  /**
+   * Register any application services.
+   *
+   * @return void
+   */
+  public function register()
+  {
+      //
+  }
+
+  /**
+   * Bootstrap any application services.
+   *
+   * @return void
+   */
+  public function boot()
+  {
+      //
+  }
 }
---
+++ app/Services/SocialAccountService.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Services;
+
+use App\User;
+use Laravel\Socialite\Two\User as ProviderUser;
+
+class SocialAccountService {
+
+  /**
+   * Find or create user instance by provider user instance and provider name.
+   * 
+   * @param ProviderUser $providerUser
+   * @param string $provider
+   * 
+   * @return User
+   */
+  public function findOrCreate(ProviderUser $providerUser, string $provider): User
+  {
+    $linkedSocialAccount = User::where('tipoCuenta', $provider)
+      ->where('providerId', $providerUser->getId())
+      ->first();
+
+    if ($linkedSocialAccount) {
+
+      return $linkedSocialAccount;
+
+    } else {
+      
+      $user = null;
+      
+      if ($email = $providerUser->getEmail()) {
+        $user = User::where('email', $email)->first();
+      }
+
+      if (!$user) {
+        $user = User::create([
+          'nombre' => $providerUser->getName(),
+          'apellido' => $providerUser->getName(),
+          'email' => $providerUser->getEmail(),
+          'tipoCuenta' => $provider,
+          'providerId' => $provider->getId(),
+          'foto' => $providerUser->getAvatar()
+        ]);
+      } else {
+        return null;
+      }
+      
+      return $user;
+    }
+  }
+}
+
+?>
