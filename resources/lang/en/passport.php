e57c6551294aa1023cc9c7d54088ea9cf80864e4
GerardoAGL96 <gerardoagl96@gmail.com>
2019-10-30 21:50:58 -0400

    Login form validation

---
+++ resources/lang/en/passport.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'invalid_credentials' => [ // key from error type
+        'error'   => 'Invalid credentials',
+        'message' => 'The user credentials were incorrect.'
+    ]
+];
+
+?>
